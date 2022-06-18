2a472a58e8ba4d499657ba646a6f22443e20bf5f
Rongshuang Gao <rongshuanggao@rongshuangs-mbp.myfiosgateway.com>
2022-06-18 17:56:03 -0400

    resolve build error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { CssBaseline, Grid } from "@material-ui/core";
 
 import { getPlacesData, getWeatherData } from "./api";
 
-import "./app.css";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
